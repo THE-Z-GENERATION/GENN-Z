0b7955a29ad186c5c6c49118531a88d307a4c17b
sheikh-kalim <sheikhkalimulla01@gmail.com>
2025-02-20 16:22:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-fetch("https://GENN-Z.infinityfreeapp.com/backend.php")
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // Backend se data frontend pe aa raha hai
-  })
-  .catch(error => console.error("Error:", error));
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var formData = new FormData(this);
+
+    fetch("https://GENN-Z.infinityfreeapp.com/backend.php", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json()) // JSON format me response le raha hai
+    .then(data => {
+        console.log(data); // Backend se data frontend pe aa raha hai
+        document.getElementById("response").innerText = data.message;
+    })
+    .catch(error => console.error("Error:", error));
+});
